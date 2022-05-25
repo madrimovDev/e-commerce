fd5864fcdfb7ff0f86ee1673c776e6a7b4a6d019
madrimov <madrimov5014>
2022-05-25 18:56:04 +0500

    starting project

---
+++ src/Routers.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import HomePage from "./pages/HomePage";
+
+const Routers = () => {
+  return (
+    <Fragment>
+      <HomePage />
+    </Fragment>
+  );
+};
+
+export default Routers;
