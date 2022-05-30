173168e456ee294121710874c0868bca4d6ad534
madrimov <madrimov5014>
2022-05-30 19:04:33 +0500

    categories

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar";
-import { useAllProduct } from "../contexts/AllProducts";
 
 export const MainLayout = () => {
-  const { isError, isLoading, products } = useAllProduct();
-
   return (
     <div>
       <Navbar />
