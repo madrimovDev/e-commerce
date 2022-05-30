e56fc875b3c8fe82d49ff008895272610af18866
madrimov <madrimov5014>
2022-05-30 16:58:41 +0500

    services

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar";
+import query from "../services/api/api.service";
 
 export const MainLayout = () => {
   return (
---
+++ src/services/api/api.service.js
@@ -0,0 +1,21 @@
+import axios from "axios"
+
+const api = axios.create({
+  baseURL: "https://fakestoreapi.com"
+})
+
+const query = {
+
+  getAllProducts: async () => {
+    try{
+      let result = await api.get("/products")
+      return result
+    } catch(error){
+      console.log(error)
+      return error
+    }
+  }
+
+}
+
+export default query
