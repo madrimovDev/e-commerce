6bdb7db5a4bb2b218e2e2fda1cd0f289e7d53f6a
Madrimov <72421738+madrimovDev@users.noreply.github.com>
2022-05-31 23:30:38 +0500

    Revert "Main"

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -2,18 +2,13 @@ import { Fragment } from "react";
 import { Route, Routes } from "react-router-dom";
 import { MainLayout } from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
-import { Products } from "./pages/HomePage/Products";
 
 const Routers = () => {
   return (
     <Fragment>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route path="/" element={<HomePage />}>
-            <Route path="/" element={<Products />} />
-            <Route path="/:id" element={<Products />} />
-          </Route>
-          <Route path="/cart" element={<>Cart</>} />
+          <Route path="/" element={<HomePage />} />
         </Route>
       </Routes>
     </Fragment>
